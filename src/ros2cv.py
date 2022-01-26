d0cc89217e18a97fa658bab3e46d749bcb6e2d7f
root <rkdms4031@unist.ac.kr>
2022-01-26 13:44:22 +0000

    succeeded to save images

--- src/ros2cv.py
+++ src/ros2cv.py
@@ -9,15 +9,22 @@ import cv2
 from std_msgs.msg import String, Int32MultiArray
 from sensor_msgs.msg import Image 
 from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Empty
+import os
 
 class image_converter:
 
 	def __init__(self):
-		self.image_pub = rospy.Publisher("/gaeun/usb_cam/image_raw_2", Image)
-		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber("/gaeun/usb_cam/image_raw", Image, self.callback)
-		# Q. Why the subscriber is defined later than the publisher? No maters?
 		self.shots = 1
+		self.image_path = ''
+		self.bridge = CvBridge()
+		self.image_pub = rospy.Publisher("/gaeun/usb_cam/image_raw_2", Image)
+		self.image_sub = rospy.Subscriber("/gaeun/usb_cam/image_raw", Image, self.callback)
+		self.reached_signal_sub = rospy.Subscriber("/reached_signal", Empty, self.callback_reached_signal)
+		
+		
+		# Q. Why the subscriber is defined later than the publisher? No maters?
+		self.reached_signal = False
 		
 	def callback(self, data):
 		try:
@@ -30,16 +37,28 @@ class image_converter:
 			cv2.circle(cv_image, (50, 50), 10, 255)
 			
 		cv2.imshow("Image window", cv_image)
-		my_input = cv2.waitKey(3)
-		print("my_input:", my_input)
-		if my_input != -1:
-			shots += 1
-			cv2.imwrite('/res/capture' + self.shots + '.jpg', cv_image)
+		cv2.waitKey(3)
 		
 		try:
 			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
 		except CvBridgeError as e :
 			print(e)
+			
+		# save an image if received signal
+		if self.reached_signal == True:
+			self.image_path = "/root/catkin_ws/src/ros_cv_pkg/res/saved_image" + str(self.shots) + ".jpeg"
+			if (cv2.imwrite(self.image_path, cv_image)): 
+				print("LOG::image is saved at " + self.image_path)
+			else: 
+				print("ERROR::image is not saved well")
+			print(os.getcwd())
+			self.reached_signal = False
+			self.shots += 1
+
+	def callback_reached_signal(self, data):
+		print('LOG::callback_reached_signal')
+		self.reached_signal = True
+		
 		
 def main(args):
 	ic = image_converter()
